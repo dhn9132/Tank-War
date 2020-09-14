8e69fa3ef0c97fde9d757b9a31985bf33809c63b
913219642@qq.com <913219642@qq.com>
2020-09-14 19:57:22 +0800

    Tank War

---
+++ src/com/dhnsoft/tank/TankFrame.java
@@ -0,0 +1,28 @@
+package com.dhnsoft.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    int x=200,y=200;
+    public TankFrame() throws HeadlessException {
+        setSize(800,600);
+        setTitle("Tank War");
+        setResizable(false);
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x,y,50,50);
+        x+=10;
+        y+=10;
+    }
+}
